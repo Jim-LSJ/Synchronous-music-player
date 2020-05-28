f667b8d15161002bedfec5665a27f59198592a1d
Jim-LSJ <jim861026@gmail.com>
2020-05-28 23:32:16 +0800

    synchronous

--- synchron_client_right.py
+++ synchron_client_right.py
@@ -4,8 +4,9 @@ import socket
 import time, datetime
 
 pygame.mixer.init()
+pygame.mixer.music.load(os.path.join('sound', 'right.wav'))
 
-HOST = '140.112.244.208'
+HOST = '192.168.137.1'
 PORT = 12200
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,22 +16,20 @@ while True:
 
     server_clock = int(server.recv(1024).decode())
 
-    client_clock = round(datetime.datetime.now().timestamp() * 1000) + 300
-    time.sleep(0.1)
+    client_clock = round(datetime.datetime.now().timestamp() * 1000)
+
     server.send( str(client_clock).encode() )
 
 
     start_time = int(round(float(server.recv(1024).decode())))
     
-    while True:
-        if round(datetime.datetime.now().timestamp() * 1000) + 300 >= start_time:
-            pygame.mixer.music.load(os.path.join('sound', 'right.wav'))
-            pygame.mixer.music.play()
-            while pygame.mixer.music.get_busy() == True:
-                continue
-        else:
-            pass
-  
+    while round(datetime.datetime.now().timestamp() * 1000) < start_time:
+        continue
+
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    
     # Start = bool(s.recv(1024).decode())
     # Break = False
     # print(Start, type(Start))
--- synchron_server.py
+++ synchron_server.py
@@ -15,14 +15,15 @@ while True:
     s.listen(5)
 
     print('The server is running')
-    
+    max_time = 0
+
     if len(client_manager) != 2:
         try:
             client, address = s.accept()
             print('Connect to {}'.format(address))
 
             server_clock = round(datetime.datetime.now().timestamp() * 1000)
-            time.sleep(0.1) # man-made delay
+            
             client.send( str(server_clock).encode() )
 
             client_clock = int(client.recv(1024).decode())
@@ -31,7 +32,7 @@ while True:
             client_true_clock = client_clock - round_trip_time / 2
 
             client_manager.append( (client, client_true_clock, round_trip_time) )
-
+            max_time = round_trip_time if round_trip_time > max_time else max_time
         except KeyboardInterrupt:
             client.send(str(True).encode())
             client.close()
@@ -44,7 +45,7 @@ while True:
         for cli in client_manager:
             toc = time.clock()
             loop_delay = toc - tic
-            cli[0].send( str(cli[1] + int(cli[2] / 2) + offset - round(loop_delay * 1000) ).encode() )
+            cli[0].send( str(cli[1] + int(max_time / 2) + offset - round(loop_delay * 1000) ).encode() )
         
         try:
             while True:
